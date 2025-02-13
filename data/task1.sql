90aa2d08b8f12b5bf6857a11d9717fa4951e9044
lkas96 <kwek_lawson@hotmail.com>
2025-02-13 11:54:06 +0800

    Task 1 Complete

---
+++ data/task1.sql
@@ -0,0 +1,15 @@
+create database movies;
+
+use movies;
+
+create table imdb (
+    imdb_id varchar(16),
+    vote_average float default 0,
+    vote_count int default 0,
+    release_date date,
+    revenue decimal(15,2) default 1000000,
+    budget decimal(15,2) default 1000000,
+    runtime int default 90,
+
+    constraint PK_imdbid PRIMARY KEY (imdb_id)
+);
